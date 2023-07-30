0bce83c6f880599dc86ef84ac13e0aa628ee95c4
Vu Duc Tiep <vuductiep@live.com>
2023-07-30 12:22:17 +0900

    complete product modal

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -6,6 +6,9 @@ import IconButton from "@/components/ui/icon-button"
 import { Expand, ShoppingCart } from "lucide-react"
 import Currency from "./currency"
 import { useRouter } from "next/navigation"
+import { MouseEventHandler } from "react"
+import PreviewModal from "../preview-modal"
+import usePreviewModal from "@/hooks/use-preview-modal"
 
 interface ProductCardProps {
   data: Product
@@ -15,24 +18,29 @@ const ProductCard: React.FC<ProductCardProps> = ({
   data
 }) => {
   const router = useRouter()
+  const previewModal = usePreviewModal()
+
   const handleClick = () => {
     router.push(`/product/${data.id}`)
   }
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation()
+    previewModal.onOpen(data)
+  }
 
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image 
           src={data?.images?.[0]?.url}
           alt={""}
           fill
           className="aspect-square object-cover round-md"
-          onClick={handleClick}
         />
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton 
-              onClick={handleClick}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />} 
             />
             <IconButton
